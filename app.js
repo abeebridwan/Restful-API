4fd2341599ea84c58f4b4026eeac49dbd6cc7ac8
Igor Tunde <igortunde@yahoo.com>
2020-10-29 00:30:08 +0400

    debug: next() argument passed to middleware

--- app.js
+++ app.js
@@ -1,32 +1,33 @@
 const express = require("express");  
 const app = express();
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
-// require('dotenv').config();
+require('dotenv').config();
 
 
-// const MONGO_URL = process.env.MONGO_URL_TEST;
-// const options = {
-//     useNewUrlParser: true,
-//     useCreateIndex: true,
-//     useFindAndModify: false,
-//     useUnifiedTopology: true,
-//   };
+const MONGO_URL = process.env.MONGO_URL_TEST;
+const options = {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+};
 
 //middleware
-app.use('/', () => {
+app.use('/', (res, req, next) => {
  console.log("This is a middleware")
-})
+ next()
+});
 
 //route
 app.get('/', (req, res) => {
-    res.send('we ar on home'); 
+    res.send('we are on home'); 
 });
 
-// database connection
-// mongoose.connect(MONGO_URL, () => {
-//     console.log("connected to Mongodb")
-// });
+//database connection
+mongoose.connect(MONGO_URL, options, () => {
+    console.log("connected to Mongodb")
+});
 
 //listen
 app.listen(3000);
