9404139cb82d7586042bd31f7fad5f3ca98e62e4
Igor Tunde <igortunde@yahoo.com>
2020-10-28 23:40:21 +0400

    connected to the datatbase

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
-const express = require("express");
-
+const express = require("express");  
 const app = express();
+// const mongoose = require('mongoose');
+
+// require('dotenv').config();
+
+
+// const MONGO_URL = process.env.MONGO_URL_TEST;
+// const options = {
+//     useNewUrlParser: true,
+//     useCreateIndex: true,
+//     useFindAndModify: false,
+//     useUnifiedTopology: true,
+//   };
+
 //middleware
 app.use('/', () => {
  console.log("This is a middleware")
@@ -11,6 +23,10 @@ app.get('/', (req, res) => {
     res.send('we ar on home'); 
 });
 
+// database connection
+// mongoose.connect(MONGO_URL, () => {
+//     console.log("connected to Mongodb")
+// });
 
 //listen
 app.listen(3000);
