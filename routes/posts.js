7f7da258b0a4647aa9bc0d04e1ba3d9a371f6bae
Igor Tunde <igortunde@yahoo.com>
2020-10-31 20:38:00 +0400

    feat: added get back all the posts refactor: submit a post

--- routes/posts.js
+++ routes/posts.js
@@ -2,27 +2,34 @@ const express = require("express");
 const router = express.Router();
 const Post = require('../model/Post');
 
-router.get('/', (req, res) => {
-    res.send('we are on post')
+// get back all the posts
+router.get('/', async (req, res) => {
+    try{
+        const posts = await Post.find();
+        res.json(posts);
+    } catch (err) {
+        res.json({message: err});
+    } 
 });
 
+// get specific page
 router.get('/specific', (req, res,) => {
     res.send('we are on specific')
 });
 
-router.post('/', (req,res) => {
+// submit a post
+router.post('/', async (req,res) => {
     const post = new Post({
         title: req.body.title,
         description: req.body.description
     });
     
-    post.save()
-    .then(data => {
-        res.json(data);
-    })
-    .catch(err => {
-        res.json({message: err})
-    })
+    try{
+        const savedPost = await post.save();
+        res.json(savedPost);
+    } catch (err) {
+        res.json({message: err});
+    } 
 }); 
 
 module.exports = router;
