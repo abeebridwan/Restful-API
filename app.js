412c0c4e12b3b7dc32057ea0a4565440c3cdf700
Igor Tunde <igortunde@yahoo.com>
2020-11-02 22:11:24 +0400

    feat: added cors
          It makes the Rest API accesible externally.

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser'); 
-
+const cors = require('cors');
 require('dotenv').config();
 
 //Routes
@@ -18,7 +18,7 @@ const options = {
 };
 
 //middleware
-
+app.use(cors());
 app.use(bodyParser.json());
 
 app.use('/posts', postsRoutes)
